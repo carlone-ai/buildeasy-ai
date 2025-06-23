3eb23f2faf94ac709bc973e0a27408ff5bccb57f
Carlone Scott <carlone@buildeasy.ai>
2025-06-23 14:10:33 -0400

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     unoptimized: true,
-//   },
-// }
-
-// export default nextConfig
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   trailingSlash: true,
-//   images: {
-//     unoptimized: true
-//   }
-// };
-
-// export default nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
